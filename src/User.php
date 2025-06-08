0b7ef0215a1828b365c25b012499f57f9d0a220b
Dimitris Kastaniotis <d.kastaniotis@iconic.gr>
2025-06-08 17:37:53 +0300

    fixed entity

--- src/User.php
+++ src/User.php
@@ -7,6 +7,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+#[ORM\Entity]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]

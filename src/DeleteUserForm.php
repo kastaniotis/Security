cb689edcf7127be9d7ed4bdefa59a04a12b2ee39
Dimitris Kastaniotis <d.kastaniotis@iconic.gr>
2025-06-09 16:19:44 +0300

    foprgot action

--- src/DeleteUserForm.php
+++ src/DeleteUserForm.php
@@ -8,10 +8,19 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\RouterInterface;
 
 class DeleteUserForm extends AbstractType
 {
+    public function __construct(private RouterInterface $router)
+    {
+
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -21,8 +30,14 @@ class DeleteUserForm extends AbstractType
                     'class' => 'btn btn-danger',
                 ],
                 'label' => 'Delete',
-            ])
-            ->setAction('delete');
+            ]);
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $data = $event->getData();
+            $form = $event->getForm();
+
+            $form->getConfig()->getOptions()['attr']['action'] = $this->router->generate('delete', ['id' => $data->getId()]);
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
